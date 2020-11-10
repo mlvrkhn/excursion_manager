9eab05fb525963a0c55fe7d19d24d066de45e3c5
Martin Gawlyta <martin.guide@gmail.com>
2020-11-11 00:30:51 +0100

    Add data Validation

---
+++ src/js/DataValidator.js
@@ -0,0 +1,35 @@
+class DataValidator {
+    
+    _validateOrder(val1, val2) {
+        if (typeof val1 === 'number' && typeof val2 === 'number') {
+            return true;
+        } else {
+            throw new Error('Both values must be numbers!')
+        }
+    }
+    _validateCustomerData(name, mail) {
+        const validName = this._validateName(name);
+        const validMail = this._validateMail(mail);
+        if (validMail && validName) {
+            return true;
+        } else {
+            return;
+        }
+    };
+    _validateName(name) {
+        if (typeof name !== 'string' || name.length < 6 || name.includes(' ')) {
+            return true
+        } else {
+            throw new Error('Wrong name!');
+        }
+    };
+    _validateMail(mail) {
+        if (!mail.includes('@') || !mail.includes('.') || mail.length < 8) {
+            throw new Error('Invalid email address!');
+        } else {
+            return true;
+        }
+    }
+}
+
+export default DataValidator;
