aca724b9be62120d0386ab90e4d94a24d751bbb5
Martin Gawlyta <martin.guide@gmail.com>
2020-11-16 22:47:26 +0100

    Divide _displayOrderSummary() onto smaller functions

--- src/js/ExcursionsManager.js
+++ src/js/ExcursionsManager.js
@@ -7,9 +7,7 @@ class ExcursionManager {
     constructor(
         excRoot = '.panel__excursions',
         excProto = '.excursions__item--prototype',
-        ordProto = '.summary__item--prototype')
-        
-        {
+        ordProto = '.summary__item--prototype') {
         this.excursionRoot = document.querySelector(excRoot);
         this.excursionProto = document.querySelector(excProto);
         this.orderProto = document.querySelector(ordProto);
@@ -37,53 +35,6 @@ class ExcursionManager {
             })
         });
     };
-
-    _editExcursion(element) {
-        const editForm = this._createEditForm();
-        const root = document.querySelector('body');
-        root.appendChild(editForm);
-        this._blurBackground(true);
-
-        editForm.addEventListener('submit', click => {
-            click.preventDefault();
-
-            const elements = click.target.elements;
-            const id = element.dataset.id;
-
-            const dataToUpdate = {
-                name: elements.name.value,
-                description: elements.description.value,
-                adultPrice: elements.price__adult.value,
-                childPrice: elements.price__child.value
-            }
-
-            editForm.classList.remove('form__active');
-            this._blurBackground(false);
-            api.editExcursion(id, dataToUpdate).then(() => {
-                const excursions = api.getExcursions();
-                this.renderExcursions(excursions);
-            }).catch(err => console.log(err));
-        });
-    }
-
-    _removeExcursion(idToDelete) {
-        if (idToDelete && idToDelete > 0) {
-            api.deleteExcursion(idToDelete).then(() => {
-                this.renderExcursions();
-            });
-        }
-    }
-
-    _createEditForm() {
-        const editForm = document.querySelector('.form').cloneNode(true);
-        editForm.classList.add('form__edit', 'form__active');
-        const editInfo = 'Enter new excursion data and click enter';
-        const editHeader = document.createElement('h3');
-        editHeader.innerText = editInfo;
-        editForm.prepend(editHeader);
-        return editForm;
-    };
-
     renderExcursions() {
         const prototype = this.excursionProto.cloneNode(true);
         this._clearExcursionView();
@@ -101,29 +52,6 @@ class ExcursionManager {
             });
         });
     };
-
-    _getExcursionProto() {
-        return (document.querySelector(".excursions__item--prototype") ||
-            document.querySelector(".excursions__item"))
-    };
-
-    _clearExcursionView() {
-        document.querySelector('.excursions').innerHTML = '';
-    };
-
-    _blurBackground(state) {
-        const body = document.querySelector('section');
-
-        if (!state) {
-            return body.classList.remove('background__blur');
-        } else if (state) {
-            return body.classList.add('background__blur');
-        }
-        return;
-    };
-
-    // ******* ORDERS ********
-
     removeFromBasket() {
         const summary = document.querySelector('.summary');
 
@@ -138,7 +66,6 @@ class ExcursionManager {
             }
         });
     };
-
     renderOrders() {
         const prototype = this.orderProto.cloneNode(true);
         const root = document.querySelector('.summary');
@@ -173,7 +100,115 @@ class ExcursionManager {
         });
         this._updateBasketTotal()
     };
+    addToBasket() {
+        const excursions = document.querySelector('.panel__excursions');
+        excursions.addEventListener('click', event => {
+            event.preventDefault();
 
+            if (event.target.value === 'dodaj do zamówienia') {
+                this._addOrderToServer(event);
+            }
+            return;
+        });
+    };
+    confirmOrder() {
+        const sendOrderBtn = document.querySelector('.order__field-submit');
+        sendOrderBtn.addEventListener('click', e => {
+            e.preventDefault();
+
+            const name = document.querySelector('input[name="name"]').value;
+            const mail = document.querySelector('input[name="email"]').value;
+            const price = document.querySelector('.order__total-price-value').innerText;
+            const validCustomerData = validator.validateCustomerData(name, mail);
+
+            if (validCustomerData) {
+                api.getOrders().then(orders => {
+                    const basket = [];
+                    orders.forEach(order => {
+                        const {
+                            name,
+                            nrAdult,
+                            nrChild
+                        } = order;
+                        const excursion = `${name}.\n Dorośli: ${nrAdult}, dzieci: ${nrChild}. Suma: ${price}.`;
+                        basket.push(excursion);
+                    })
+                    this._displayOrderSummary(name, mail, price, basket);
+                })
+            } else {
+                throw new Error('Invalid data');
+            }
+        });
+    };
+    listenForAddExcursion() {
+        const newExcursionForm = document.querySelector('.form');
+
+        newExcursionForm.addEventListener('submit', e => {
+            e.preventDefault();
+            const excursion = this._createExcursion(e);
+            api.addExcursion(excursion).then(() => this.renderExcursions())
+        });
+    };
+    _editExcursion(element) {
+        const editForm = this._createEditForm();
+        const root = document.querySelector('body');
+        root.appendChild(editForm);
+        this._blurBackground(true);
+
+        editForm.addEventListener('submit', click => {
+            click.preventDefault();
+
+            const elements = click.target.elements;
+            const id = element.dataset.id;
+
+            const dataToUpdate = {
+                name: elements.name.value,
+                description: elements.description.value,
+                adultPrice: elements.price__adult.value,
+                childPrice: elements.price__child.value
+            }
+
+            editForm.classList.remove('form__active');
+            this._blurBackground(false);
+            api.editExcursion(id, dataToUpdate).then(() => {
+                const excursions = api.getExcursions();
+                this.renderExcursions(excursions);
+            }).catch(err => console.log(err));
+        });
+    }
+    _removeExcursion(idToDelete) {
+        if (idToDelete && idToDelete > 0) {
+            api.deleteExcursion(idToDelete).then(() => {
+                this.renderExcursions();
+            });
+        }
+    }
+    _createEditForm() {
+        const editForm = document.querySelector('.form').cloneNode(true);
+        editForm.classList.add('form__edit', 'form__active');
+        const editInfo = 'Enter new excursion data and click enter';
+        const editHeader = document.createElement('h3');
+        editHeader.innerText = editInfo;
+        editForm.prepend(editHeader);
+        return editForm;
+    };
+    _getExcursionProto() {
+        return (document.querySelector(".excursions__item--prototype") ||
+            document.querySelector(".excursions__item"))
+    };
+    _clearExcursionView() {
+        document.querySelector('.excursions').innerHTML = '';
+    };
+    _blurBackground(state) {
+        const body = document.querySelector('section');
+
+        if (!state) {
+            return body.classList.remove('background__blur');
+        } else if (state) {
+            return body.classList.add('background__blur');
+        }
+        return;
+    };
     _clearOrdersView() {
         const root = document.querySelector('.summary');
         while (root.childNodes.length > 1) {
@@ -190,64 +225,60 @@ class ExcursionManager {
 
     _displayOrderSummary(name, email, price, orders) {
         const root = document.querySelector('body');
-
         const popUpSummary = document.createElement('div');
-        const titleSummary = document.createElement('h3');
-        const infoSummary = document.createElement('p');
-        const orderSummaryList = document.createElement('ul');
-        const totalSummarySum = document.createElement('p');
-        const eMailAddress = document.createElement('p');
 
-        this._createElementWithText('div', text, appendTo)
-        this._createElementWithText('div', text, appendTo)
-        this._createElementWithText('div', text, appendTo)
-        this._createElementWithText('div', text, appendTo)
-        this._createElementWithText('div', text, appendTo)
-        this._createElementWithText('div', text, appendTo)
-
-        titleSummary.textContent = `Congrats ${name}!`;
-        totalSummarySum.textContent = price;
-        eMailAddress.textContent = `We will send the tickets to ${email}`;
-
-        infoSummary.textContent = `You have ordered following excursions:`;
-        orders.forEach(ord => {
+        const title = `Congrats ${name}!`;
+        const summary = `You have ordered following excursions:`;
+        const mailInfo = `We will send the tickets to ${email}`;
+        const listItem = orders.forEach(ord => {
             const el = document.createElement('li');
             el.textContent = ord;
             orderSummaryList.appendChild(el);
         })
 
+        this._createElementWithText('h3', title, popUpSummary)
+        this._createElementWithText('p', summary, popUpSummary)
+        this._createElementWithText('ul', listItem, popUpSummary)
+        this._createElementWithText('p', price, popUpSummary)
+        this._createElementWithText('p', mailInfo, popUpSummary)
+
         popUpSummary.classList.add('form__edit', 'form__active');
-        popUpSummary.appendChild(titleSummary);
-        popUpSummary.appendChild(infoSummary);
-        popUpSummary.appendChild(orderSummaryList);
-        popUpSummary.appendChild(totalSummarySum);
-        popUpSummary.appendChild(eMailAddress);
         root.appendChild(popUpSummary);
 
         this._blurBackground(true);
-
-        if (popUpSummary) {
+        this._hideSummary(popUpSummary);
+    };
+    _createExcursion(event) {
+        const {
+            target: {
+                elements
+            }
+        } = event;
+        const excursion = {
+            name: elements.name.value,
+            description: elements.description.value,
+            adultPrice: elements.price__adult.value,
+            childPrice: elements.price__child.value,
+        };
+        return excursion;
+    }
+    _createElementWithText(elementToCreate, txtContent, rootElement) {
+        const element = document.createElement(elementToCreate);
+        element.textContent = txtContent;
+        rootElement.appendChild(element);
+    }
+    _hideSummary(element) {
+        if (element) {
             document.querySelector('body').addEventListener('click', () => {
-                popUpSummary.classList.remove('form__active');
+                element.classList.remove('form__active');
                 this._blurBackground(false);
                 api.removeAllOrders();
                 document.querySelector('.summary').innerHTML = '';
             });
-        }
-    };
-
-    addToBasket() {
-        const excursions = document.querySelector('.panel__excursions');
-        excursions.addEventListener('click', event => {
-            event.preventDefault();
-
-            if (event.target.value === 'dodaj do zamówienia') {
-                this._addOrderToServer(event);
-            }
+        } else {
             return;
-        });
-    };
-
+        }
+    }
     _addOrderToServer(event) {
         const curr = event.target;
         const root = curr.parentNode.parentNode.parentNode;
@@ -278,62 +309,6 @@ class ExcursionManager {
             return;
         }
     };
-
-    confirmOrder() {
-        const sendOrderBtn = document.querySelector('.order__field-submit');
-        sendOrderBtn.addEventListener('click', e => {
-            e.preventDefault();
-
-            const name = document.querySelector('input[name="name"]').value;
-            const mail = document.querySelector('input[name="email"]').value;
-            const price = document.querySelector('.order__total-price-value').innerText;
-
-            const validCustomerData = validator.validateCustomerData(name, mail);
-
-            if (validCustomerData) {
-                api.getOrders().then(orders => {
-                    const basket = [];
-                    orders.forEach(order => {
-                        const {
-                            name,
-                            nrAdult,
-                            nrChild
-                        } = order;
-                        const excursion = `${name}.\n Dorośli: ${nrAdult}, dzieci: ${nrChild}. Suma: ${price}.`;
-                        basket.push(excursion);
-                    })
-                    this._displayOrderSummary(name, mail, price, basket);
-                })
-            } else {
-                throw new Error('Invalid data');
-            }
-        });
-    };
-
-    listenForAddExcursion() {
-        const newExcursionForm = document.querySelector('.form');
-
-        newExcursionForm.addEventListener('submit', e => {
-            e.preventDefault();
-            const excursion = this._createExcursion(e);
-            api.addExcursion(excursion).then(() => this.renderExcursions())
-        });
-    };
-
-    _createExcursion(event) {
-        const {
-            target: {
-                elements
-            }
-        } = event;
-        const excursion = {
-            name: elements.name.value,
-            description: elements.description.value,
-            adultPrice: elements.price__adult.value,
-            childPrice: elements.price__child.value,
-        };
-        return excursion;
-    }
 }
 
 export default ExcursionManager;
